a575762229f0f18086e77dd29319078e42c7206f
hirotta <is0427rv@ed.ritsumei.ac.jp>
2019-02-20 19:23:21 +0900

    bug fix

---
+++ getLocation/parse_json.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+import json
+import sys
+
+args = sys.argv
+
+file_data = open(args[1], "r")
+
+keys = ["name", "lat", "lng", "compound_code", "global_code", "say"]
+data_dict = {}
+for k in keys:
+    data_dict[k] = None
+
+for f in file_data:
+    strip_str = f.lstrip()
+    for k in keys:
+        key_len = len(k)
+        temp_str = strip_str[1: key_len+1]
+        if temp_str == k:
+            if data_dict[k] == None:
+                if k != "compound_code":
+                    #データが文字列か数字かで削る文字数を変える
+                    if(strip_str[key_len+5] == '"'):
+                        data_dict[k] = strip_str[key_len + 6:-3] #:と空白と"、,と改行コードを消す
+                    else:
+                        data_dict[k] = strip_str[key_len + 5:-2] #:と空白、
+                else:
+                    temp_prefecture = strip_str[key_len + 6:-3].split("、")
+                    data_dict[k] = temp_prefecture[-1]
+                print(strip_str,end="")
+
+print("\n------Dict Data------")
+print(data_dict)
+
+print("\n------Json Data------")
+json_data =  json.dumps(data_dict, ensure_ascii=False)
+print(json_data)
+
+with open("Data_" + args[1], "w") as fw:
+    fw.write(json_data)
+
+print("end")
