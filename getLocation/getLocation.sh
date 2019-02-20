203f45669e66bdf50bd6e0acb72ab02972ce6c63
hirotta <is0427rv@ed.ritsumei.ac.jp>
2019-02-20 13:25:21 +0900

    jsonデータを取得

--- getLocation/getLocation.sh
+++ getLocation/getLocation.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 URL="https://maps.googleapis.com/maps/api/place/textsearch/json";
 QUERY=("海遊館" "USJ" "万博記念公園" "通天閣" "大阪城" "住吉大社" "あべのハルカス" "グランフロント大阪" "なんばグランド花月" "嵐山" "清水寺" "金閣寺" "伏見稲荷大社");
@@ -8,5 +8,5 @@ DIR=".";
 SETDT=`date "+%%m%d%H%M%S"`;
 
 for((i = 0; i < ${#QUERY[@]}; i++)){
-	curl -o ${QUERY[i]}.json $URL?query=${QUERY[i]}\&key=$KEY;
+	curl -o ${QUERY[i]}.json $URL?query=${QUERY[i]}\&key=$KEY\&language=ja;
 }
